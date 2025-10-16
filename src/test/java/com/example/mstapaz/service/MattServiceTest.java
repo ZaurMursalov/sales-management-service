3757f274fa0ca133196ef4529b1ccda4a0c1ac06
ZaurMursalov <zaurmursalov44@gmail.com>
2025-10-16 22:56:29 +0400

    add some files

---
+++ src/test/java/com/example/mstapaz/service/MattServiceTest.java
@@ -0,0 +1,12 @@
+package com.example.mstapaz.service;
+
+import org.hibernate.service.spi.InjectService;
+import org.mockito.InjectMocks;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MattServiceTest {
+    @InjectMocks
+    MattService mattService;
+
+}
